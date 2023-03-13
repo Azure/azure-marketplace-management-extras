320b2d618d5d8f45bcc311ebbcb0676251593933
polinao <polina.olemskaia@gmail.com>
2023-03-13 15:51:28 +0000

    async version

--- policy-states-collector/PolicyStates/__init__.py
+++ policy-states-collector/PolicyStates/__init__.py
@@ -1,19 +1,29 @@
 from azure.mgmt.policyinsights.aio import PolicyInsightsClient
-
 from azure.mgmt.policyinsights.models import QueryOptions
 from azure.identity.aio import DefaultAzureCredential
-# from azure.identity import ClientSecretCredential
 from azure.identity.aio import ClientSecretCredential
 import azure.functions as func
 import asyncio
+
+from azure.monitor.ingestion.aio import LogsIngestionClient
+from azure.monitor.ingestion import UploadLogsStatus
+from azure.data.tables.aio import TableClient
+
 import logging
 import os
 import json
-from azure.monitor.ingestion import LogsIngestionClient
-from azure.monitor.ingestion import UploadLogsStatus
+import time
 from typing import List
-# from azure.data.tables import TableClient
-from azure.data.tables.aio import TableClient
+
+DATA_COLLECTION_ENDPOINT = str(os.environ["DATA_COLLECTION_ENDPOINT"])
+DATA_COLLECTION_IMMUTABLE_ID = str(
+    os.environ["DATA_COLLECTION_IMMUTABLE_ID"])
+STREAM_NAME = str(os.environ["STREAM_NAME"])
+AZURE_TENANT_ID = str(os.environ["AZURE_TENANT_ID"])
+AZURE_CLIENT_ID = str(os.environ["AZURE_CLIENT_ID"])
+AZURE_CLIENT_SECRET = str(os.environ["AZURE_CLIENT_SECRET"])
+CONNECTION_STRING = str(os.environ["CONNECTION_STRING"])
+TABLE_NAME = str(os.environ["TABLE_NAME"])
 
 
 async def get_resource_group_policies(policy_client, subscription_id, resource_group_name):
@@ -30,77 +40,76 @@ async def get_resource_group_policies(policy_client, subscription_id, resource_g
         query_options=query_options)
 
 
-async def get_policies(policy_client, subscription_id, resource_group_name):
+async def get_policies(client_credential, subscription_id, resource_group_name):
     try:
-        policy_assignment_states = await get_resource_group_policies(policy_client, subscription_id, resource_group_name)
+        async with PolicyInsightsClient(
+                client_credential, subscription_id=subscription_id) as policy_client:
 
-        # Build up body object with only necessary values
-        contoso_policies: List[dict] = []
+            policy_assignment_states = await get_resource_group_policies(policy_client, subscription_id, resource_group_name)
 
-        async for policy in policy_assignment_states:
-            # if policy.policy_assignment_name.startswith('myprefix-'):
-            contoso_policies.append({
-                'Policy_assignment_name': policy.policy_assignment_name,
-                'Policy_assignment_id': policy.policy_assignment_id,
-                'Is_compliant': policy.is_compliant,
-                'TimeGenerated': json.dumps(policy.timestamp, default=str)
-            })
+            # Build up body object with only necessary values
+            contoso_policies: List[dict] = []
 
-        if len(contoso_policies) == 0:
-            logging.warning("There are not any contoso policies")
-        else:
-            return contoso_policies
+            async for policy in policy_assignment_states:
+                # if policy.policy_assignment_name.startswith('myprefix-'):
+                contoso_policies.append({
+                    'Policy_assignment_name': policy.policy_assignment_name,
+                    'Policy_assignment_id': policy.policy_assignment_id,
+                    'Is_compliant': policy.is_compliant,
+                    'TimeGenerated': json.dumps(policy.timestamp, default=str)
+                })
+
+            logging.info(
+                f"Contoso policies amount: {str(len(contoso_policies))} for RG {resource_group_name}")
+
+            if len(contoso_policies) == 0:
+                logging.warning("There are not any contoso policies")
+            else:
+                return contoso_policies
     except Exception as e:
         msg = f"Failed to get policies for RG {resource_group_name}, error: {e}"
         logging.error(msg)
 
 
 async def run():
-
     all_applications_policies_to_upload = []
-    # async with TableClient.from_connection_string(CONNECTION_STRING, TABLE_NAME) as table_client:
-    #     managed_applications = table_client.query_entities(
-    #         "xTenant eq 'true'", select=['resource_group_name', 'subscription_id'])
-
-    #     async for managed_app in managed_applications:
-    #         logging.info("managed app resource group " +
-    #                      managed_app["resource_group_name"] + "    " + managed_app["subscription_id"])
-    #         result = get_policies(managed_app)
-    #         logging.info(str(result))
-    #         all_applications_policies_to_upload.append(result)
 
     async with ClientSecretCredential(
-        AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET) as client_credential, PolicyInsightsClient(
-            client_credential, subscription_id=sid) as policy_client:
-        for application in applications:
+        AZURE_TENANT_ID,
+        AZURE_CLIENT_ID, AZURE_CLIENT_SECRET
+    ) as client_credential, TableClient.from_connection_string(
+        CONNECTION_STRING, TABLE_NAME
+    ) as table_client:
+        managed_applications = table_client.query_entities(
+            "xTenant eq 'true'", select=['resource_group_name', 'subscription_id'])
+
+        async for application in managed_applications:
             result = get_policies(
-                policy_client, application["subscription_id"], application["resource_group_name"])
+                client_credential, application["subscription_id"], application["resource_group_name"])
             all_applications_policies_to_upload.append(result)
 
         results = await asyncio.gather(*all_applications_policies_to_upload, return_exceptions=True)
         logging.info("results " + str(results))
 
-    # # Upload policies
-    # with DefaultAzureCredential() as default_credential:
-    #     try:
-    #         client = LogsIngestionClient(
-    #             endpoint=DATA_COLLECTION_ENDPOINT, credential=default_credential, logging_enable=True)
-
-    #         # https://learn.microsoft.com/en-us/azure/azure-monitor/logs/tutorial-logs-ingestion-portal#sample-data
-    #         response = client.upload(
-    #             rule_id=DATA_COLLECTION_IMMUTABLE_ID, stream_name=STREAM_NAME, logs=all_applications_policies_to_upload)
-    #         if response.status != UploadLogsStatus.SUCCESS:
-    #             failed_logs = response.failed_logs_index
-    #             msg = f"Failed to send data to Log Analytics: {failed_logs}"
-    #             logging.error(msg)
-    #             return msg
-    #     except Exception as e:
-    #         msg = f"Failed to upload policies: {e}"
-    #         logging.error(msg)
-    #         return msg
+    # Upload policies
+    async with DefaultAzureCredential() as default_credential, LogsIngestionClient(
+            endpoint=DATA_COLLECTION_ENDPOINT, credential=default_credential, logging_enable=True) as logs_client:
+        response = await logs_client.upload(
+            rule_id=DATA_COLLECTION_IMMUTABLE_ID, stream_name=STREAM_NAME, logs=[1])
+        if response.status != UploadLogsStatus.SUCCESS:
+            failed_logs = response.failed_logs_index
+            msg = f"Failed to send data to Log Analytics: {failed_logs}"
+            logging.error(msg)
+            return msg
+        else:
+            logging.info("-------------COMPLETED------------")
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    logging.info("Triggered by timer")
+    logging.info("-------------TRIGGERED BY TIMER-------------")
+    tic = time.perf_counter()
     asyncio.run(run())
-    logging.info("Done")
+    toc = time.perf_counter()
+    logging.info(
+        f'took {toc - tic:0.4f} seconds'
+    )
