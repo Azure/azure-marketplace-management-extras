50bfc64779e3bf1553f946e1dc72462e61e792c1
polinao <polina.olemskaia@gmail.com>
2023-03-13 14:36:15 +0000

    working async

--- policy-states-collector/PolicyStates/__init__.py
+++ policy-states-collector/PolicyStates/__init__.py
@@ -1,7 +1,7 @@
 from azure.mgmt.policyinsights.aio import PolicyInsightsClient
 
 from azure.mgmt.policyinsights.models import QueryOptions
-from azure.identity import DefaultAzureCredential
+from azure.identity.aio import DefaultAzureCredential
 # from azure.identity import ClientSecretCredential
 from azure.identity.aio import ClientSecretCredential
 import azure.functions as func
@@ -15,86 +15,71 @@ from typing import List
 # from azure.data.tables import TableClient
 from azure.data.tables.aio import TableClient
 
-DATA_COLLECTION_ENDPOINT = str(os.environ["DATA_COLLECTION_ENDPOINT"])
-DATA_COLLECTION_IMMUTABLE_ID = str(
-    os.environ["DATA_COLLECTION_IMMUTABLE_ID"])
-STREAM_NAME = str(os.environ["STREAM_NAME"])
 
-
-async def get_resource_group_policies(credential, SUBSCRIPTION_ID, RESOURCE_GROUP_NAME):
-    policyClient = PolicyInsightsClient(
-        credential, subscription_id=SUBSCRIPTION_ID)
+async def get_resource_group_policies(policy_client, subscription_id, resource_group_name):
     # Do not change or remove filter. It is used to query policies specifically assigned for RG
-    scope = f"/subscriptions/{SUBSCRIPTION_ID}/resourceGroups/{RESOURCE_GROUP_NAME}"
+    scope = f"/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}"
     filter = "PolicyAssignmentScope eq '{}'".format(scope)
     query_options = QueryOptions(filter=filter)
 
     # Only need latest evaluated policies
-    return policyClient.policy_states.list_query_results_for_resource_group(
+    return policy_client.policy_states.list_query_results_for_resource_group(
         policy_states_resource='latest',
-        subscription_id=SUBSCRIPTION_ID,
-        resource_group_name=RESOURCE_GROUP_NAME,
+        subscription_id=subscription_id,
+        resource_group_name=resource_group_name,
         query_options=query_options)
 
 
-async def get_policies(managed_app, client_credential):
-    policy_assignment_states = await get_resource_group_policies(
-        client_credential, managed_app["subscription_id"], managed_app["resource_group_name"])
+async def get_policies(policy_client, subscription_id, resource_group_name):
+    try:
+        policy_assignment_states = await get_resource_group_policies(policy_client, subscription_id, resource_group_name)
 
-    # Build up body object with only necessary values
-    contoso_policies: List[dict] = []
+        # Build up body object with only necessary values
+        contoso_policies: List[dict] = []
 
-    async for policy in policy_assignment_states:
-        # if policy.policy_assignment_name.startswith('myprefix-'):
-        contoso_policies.append({
-            'Policy_assignment_name': policy.policy_assignment_name,
-            'Policy_assignment_id': policy.policy_assignment_id,
-            'Is_compliant': policy.is_compliant,
-            'TimeGenerated': json.dumps(policy.timestamp, default=str)
-        })
+        async for policy in policy_assignment_states:
+            # if policy.policy_assignment_name.startswith('myprefix-'):
+            contoso_policies.append({
+                'Policy_assignment_name': policy.policy_assignment_name,
+                'Policy_assignment_id': policy.policy_assignment_id,
+                'Is_compliant': policy.is_compliant,
+                'TimeGenerated': json.dumps(policy.timestamp, default=str)
+            })
 
-    logging.info("Contoso policies length: " + str(len(contoso_policies)))
-
-    if len(contoso_policies) == 0:
-        logging.warning("There are not any contoso policies")
-    else:
-        return contoso_policies
+        if len(contoso_policies) == 0:
+            logging.warning("There are not any contoso policies")
+        else:
+            return contoso_policies
+    except Exception as e:
+        msg = f"Failed to get policies for RG {resource_group_name}, error: {e}"
+        logging.error(msg)
 
 
 async def run():
-    try:
-        # Acquire a credential object for the app identity. When running in the cloud,
-        # DefaultAzureCredential will use system's identity that has been created as part function deployment
-        # When run locally, DefaultAzureCredential relies on environment variables named
-        # AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, and AZURE_TENANT_ID.
-        # Check the following link for more information:
-        # https://docs.microsoft.com/en-us/azure/marketplace/plan-azure-app-managed-app#choose-who-can-manage-the-application
-        # credential = DefaultAzureCredential()
-        client_credential = ClientSecretCredential(
-            AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET)
-    except Exception as e:
-        msg = f"Could not authenticate: {e}"
-        logging.error(msg)
-        return msg
 
     all_applications_policies_to_upload = []
+    # async with TableClient.from_connection_string(CONNECTION_STRING, TABLE_NAME) as table_client:
+    #     managed_applications = table_client.query_entities(
+    #         "xTenant eq 'true'", select=['resource_group_name', 'subscription_id'])
 
-    async with TableClient.from_connection_string(CONNECTION_STRING, TABLE_NAME) as table_client:
-        managed_applications = table_client.query_entities(
-            "xTenant eq 'true'", select=['resource_group_name', 'subscription_id'])
+    #     async for managed_app in managed_applications:
+    #         logging.info("managed app resource group " +
+    #                      managed_app["resource_group_name"] + "    " + managed_app["subscription_id"])
+    #         result = get_policies(managed_app)
+    #         logging.info(str(result))
+    #         all_applications_policies_to_upload.append(result)
 
-        async for managed_app in managed_applications:
-            logging.info("managed app resource group " +
-                         managed_app["resource_group_name"])
-            result = get_policies(managed_app, client_credential)
-            logging.info(str(result))
+    async with ClientSecretCredential(
+        AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET) as client_credential, PolicyInsightsClient(
+            client_credential, subscription_id=sid) as policy_client:
+        for application in applications:
+            result = get_policies(
+                policy_client, application["subscription_id"], application["resource_group_name"])
             all_applications_policies_to_upload.append(result)
 
-    policy = await asyncio.gather(*all_applications_policies_to_upload)
-    logging.info(str(policy))
-    # done, _ = await asyncio.wait(all_applications_policies_to_upload)
+        results = await asyncio.gather(*all_applications_policies_to_upload, return_exceptions=True)
+        logging.info("results " + str(results))
 
-    # logging.info(str(done.pop().result))
     # # Upload policies
     # with DefaultAzureCredential() as default_credential:
     #     try:
