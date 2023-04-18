efdda7c94a03aea41be840e571c550d3f2f3cade
polinao <polina.olemskaia@gmail.com>
2023-04-18 11:34:52 +0100

    save into db

--- applications/NotificationHandler/__init__.py
+++ applications/NotificationHandler/__init__.py
@@ -1,10 +1,15 @@
 import logging
 import re
+import os
 
 import azure.functions as func
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ApplicationClient
+from azure.data.tables import TableServiceClient
+from azure.core.exceptions import ResourceExistsError
 
+CONNECTION_STRING = str(os.environ["AzureWebJobsStorage"])
+TABLE_NAME = str(os.environ["TABLE_NAME"])
 
 def parse_application_id(resource_id: str):
     pattern = (
@@ -114,5 +119,22 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # You can check all the Application object attributes here:
     # https://github.com/Azure/azure-sdk-for-python/blob/830ccf6ab129bdd6b7343cfae39e4e8e4b3bfd4d/sdk/resources/azure-mgmt-resource/azure/mgmt/resource/managedapplications/models/_models_py3.py#L191
     logging.info(app_details)
+    details_managed_resource_group_id = app_details.managed_resource_group_id.split("/")
+
+    entity = {
+            "subscription_id": details_managed_resource_group_id[2],
+            "resource_group_name": details_managed_resource_group_id[4],
+            "PartitionKey":  details_managed_resource_group_id[2],
+            "RowKey": details_managed_resource_group_id[4]
+        }
+
+    with TableServiceClient.from_connection_string(CONNECTION_STRING) as table_service_client:
+            table_client = table_service_client.create_table_if_not_exists(table_name=TABLE_NAME)
+            logging.info("Table name: {}".format(table_client.table_name))
+            try:
+                resp = table_client.create_entity(entity=entity)
+                logging.info(resp)
+            except ResourceExistsError:
+                logging.error("Entity already exists")
 
     return func.HttpResponse("OK", status_code=200)
